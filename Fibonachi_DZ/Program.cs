1fb8134b126bc225cf58bcc078a41d203ab3c02d
Микола Матіос <mikolka09@gmail.com>
2021-02-25 14:38:21 +0200

    25.02.2021

---
+++ Fibonachi_DZ/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Fibonachi_DZ
+{
+    class Program
+    {
+        delegate List<uint> AsyncFibo(int strat, int end);
+        static void Main(string[] args)
+        {
+            //Подсчет числа Фибонначи от нуля до границы указанной пользователем
+            Console.WriteLine("\tПОДСЧЕТ ЧИСЛА ФИБОННАЧИ\n" +
+                              "\t-----------------------");
+            Console.WriteLine();
+            Console.Write(" Укажите границу диапазона (конец диапазона): ");
+            int start = 0;
+            int end = Convert.ToInt32(Console.ReadLine());
+            List<uint> result = new List<uint>(); ;
+            AsyncFibo asyncFibo = (int st, int en) => 
+            {
+                List<uint> fibo = new List<uint>();
+                uint a = 0, b = 1;
+                fibo.Add(a);
+                for (int i = st; i < en; i++)
+                {
+                    uint temp = a;
+                    a = b;
+                    b = temp + b;
+                    if (a <= en)
+                        fibo.Add(a);
+                    else
+                        break;
+                }
+                return fibo;
+            };
+
+            IAsyncResult ar = asyncFibo.BeginInvoke(start, end, null, null);
+            while (!ar.IsCompleted)
+            {
+                result = asyncFibo.EndInvoke(ar);
+            }
+            Console.WriteLine();
+            Console.WriteLine($" Числовой ряд Фибонначи от 0 до {end}: \n");
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Для выхода нажмите любую кнопку!");
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ FindWord_DZ/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace FindWord_DZ
+{
+    class Program
+    {
+        delegate int AsyncCountWord(StreamReader sr, string word);
+        static void Main(string[] args)
+        {
+            //Поиск слова в файле в некоторой директории
+            Console.WriteLine("\tПОИСК СЛОВА В ФАЙЛЕ НЕКОТОРОЙ ДИРЕКТОРИИ\n" +
+                              "\t----------------------------------------");
+            Console.WriteLine();
+            Console.Write(" Укажите слово, которе требуется найти: ");
+            string word = Console.ReadLine();
+            Console.Write(" Укажите путь к Директории: ");
+            string address = Console.ReadLine();
+            int countWord = 0;
+            if (Directory.Exists(address))
+            {
+                string[] files = Directory.GetFiles(address, "*.txt");
+                foreach (var item in files)
+                {
+                    StreamReader sr = new StreamReader(item, Encoding.Default);
+                    AsyncCountWord asyncCount = CountWord;
+                    IAsyncResult ar = asyncCount.BeginInvoke(sr, word, null, null);
+                    
+                    while (!ar.IsCompleted)
+                    {
+                        countWord = asyncCount.EndInvoke(ar);
+                        Console.WriteLine();
+                        Console.WriteLine(" ОТЧЕТ ПОИСКА\n" +
+                                          " ------------");
+                        Console.WriteLine($"Название файла: {Path.GetFileName(item)}");
+                        Console.WriteLine($"Путь к файла: {item}");
+                        Console.WriteLine($"Количество вхождений слова '{word}': {countWord}");
+                    }
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+        static int CountWord(StreamReader sr, string wr)
+        {
+            int count = 0;
+            string st = "";
+            while (!sr.EndOfStream)
+            {
+                st = sr.ReadLine();
+                foreach (var item in st.Split())
+                {
+                    if (item.Contains(wr))
+                        count++;
+                }
+            }
+            return count;
+        }
+    }
+}
